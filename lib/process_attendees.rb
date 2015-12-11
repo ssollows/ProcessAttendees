a102fa3df345e21324e38c1eb9acb63c001df256
ssollows <sollows.scott@gmail.com>
2015-12-11 15:16:38 -0800

    Restructured the file a bit and also added email validation

--- lib/process_attendees.rb
+++ lib/process_attendees.rb
@@ -1,5 +1,30 @@
 require 'active_model'
+require 'email_validator'
+require 'action_view'
+include ActionView::Helpers::NumberHelper
+require 'csv'
+require 'phone'
 
-class ProcessAttendees
-    puts 'init!';
+def init
+    valid_attendees = Array.new
+    invalid_attendees = Array.new
+    attendees = Array.new
+    keys = ['firstname', 'lastname', 'email', 'phone']
+    
+    attendees = CSV.read('attendee_data/raw_attendees.csv').map {|a| Hash[ keys.zip(a) ]}
+    attendees.each do |element|
+        normalize_phone_number(element['phone'])
+        if EmailValidator.valid?(element['email'])
+            puts 'phone is good and true'
+            valid_attendees.push(element)
+            else
+            puts 'phone is bad and false'
+            invalid_attendees.push(element)
+        end
+    end
 end
+
+def normalize_phone_number(num)
+end
+
+init()
